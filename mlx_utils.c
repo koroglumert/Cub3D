675b230593f4c7cc6c497ad78fcb0af7b13c5d64
Havva Berre Yilmaz <havyilma@k1m41s06.42istanbul.com.tr>
2023-09-14 18:03:07 +0300

    if

--- mlx_utils.c
+++ mlx_utils.c
@@ -6,7 +6,7 @@
 /*   By: havyilma <havyilma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 16:46:23 by havyilma          #+#    #+#             */
-/*   Updated: 2023/09/12 13:43:59 by havyilma         ###   ########.fr       */
+/*   Updated: 2023/09/14 13:37:31 by havyilma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,15 +56,14 @@ void	paint_cloud_and_grass(t_setting *set)
 			{	
 				test =  set->mlx->mlx_img_addr + (y * set->mlx->size_line + (x * set->mlx->bits_per_pixel / 8));
 				*(unsigned int *)test = set->map->rgb_ceiling; 
-		//		mlx_pixel_put(set->mlx->mlx_init, set->mlx->mlx_window, x , y , set->map->rgb_ceiling);
+			//	mlx_pixel_put(set->mlx->mlx_init, set->mlx->mlx_window, x * set->mlx->bits_per_pixel  , y * set->mlx->size_line , set->map->rgb_ceiling);
 			}
 			
 			else
 			{
-
 				test =  set->mlx->mlx_img_addr + (y * set->mlx->size_line + (x * set->mlx->bits_per_pixel / 8));
 				*(unsigned int *)test = set->map->rgb_floor; 
-			//	mlx_pixel_put(set->mlx->mlx_init, set->mlx->mlx_window, x, y * set->mlx->size_line, set->map->rgb_floor);
+			//	mlx_pixel_put(set->mlx->mlx_init, set->mlx->mlx_window, x * set->mlx->bits_per_pixel  , y * set->mlx->size_line, set->map->rgb_floor);
 			}
 			y++;
 		}
--- walls_and_roses.c
+++ walls_and_roses.c
@@ -6,7 +6,7 @@
 /*   By: havyilma <havyilma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 11:01:58 by havyilma          #+#    #+#             */
-/*   Updated: 2023/09/12 13:45:43 by havyilma         ###   ########.fr       */
+/*   Updated: 2023/09/14 14:03:56 by havyilma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	oh_my_walls(t_setting *set)
 		set->player->distance = set->player->sidedist_x - set->player->deltadist_x;
 	else
 		set->player->distance = set->player->sidedist_y - set->player->deltadist_y;   //dik uzaklık olmadı bence bunlar ama neyse
-	set->player->wall_height = 500;
-	set->player->beginning_of_the_walls = HEIGHT / 2 - 250;
+	set->player->wall_height = HEIGHT / set->player->distance;
+	set->player->beginning_of_the_walls = HEIGHT / 2 - set->player->wall_height / 2;
 	if (set->player->beginning_of_the_walls < 0)  // duvara çok yakın (dibinde) olma ihtimalimiz
 		set->player->beginning_of_the_walls = 0;
-	set->player->end_of_the_walls = HEIGHT / 2 + 250;
+	set->player->end_of_the_walls = HEIGHT / 2 + set->player->wall_height / 2;
 	if (set->player->end_of_the_walls >= set->map->win_i)
 		set->player->end_of_the_walls = set->map->win_i - 1;
 //	printf("%d   dist:%f  wall:%d  %d   %d\n", HEIGHT, set->player->distance, set->player->wall_height, set->player->beginning_of_the_walls, set->player->end_of_the_walls);
@@ -31,32 +31,39 @@ void	oh_my_walls(t_setting *set)
 int	put_pixel_to_address (t_img *wesn, int x, int y, t_setting *set)
 {
 	(void)set;
+	(void)wesn;
 	if (x < 0 || x > TEXT_W || y < 0 || y > TEXT_H)
 		return (0);
-	return (*(int *)(wesn->data + (y * wesn->sizeline) + x * wesn->bpp / 8));
+	return (0x00FFFF);
 }
 
-void	pixel_by_pixel (t_setting *set, int j, int y)
+void	pixel_by_pixel (t_setting *set, int y, int x)
 {
 	int		color;
-/*
-	if (set->player->face_of_cube == 'x' && set->player->raydir_y > 0)
-		color = put_pixel_to_address(&set->map->south, set->player->text_x, set->player->text_y, set);	
-	else if (set->player->face_of_cube == 'x' && set->player->raydir_y < 0)
-		color = put_pixel_to_address(&set->map->north, set->player->text_x, set->player->text_y, set);	
-	else if (set->player->face_of_cube == 'y' && set->player->raydir_x > 0)
-		color = put_pixel_to_address(&set->map->west, set->player->text_x, set->player->text_y, set);	
-	else 
-		color = put_pixel_to_address(&set->map->east, set->player->text_x, set->player->text_y, set);
-*/	
-	color = 0x00FFFF;
+
+	while (y < set->player->end_of_the_walls)
+	{
+		if (set->player->face_of_cube == 'x' && set->player->raydir_y > 0)
+			color = put_pixel_to_address(&set->map->south, x, y, set);	
+		else if (set->player->face_of_cube == 'x' && set->player->raydir_y < 0)
+			color = put_pixel_to_address(&set->map->north, x, y, set);	
+		else if (set->player->face_of_cube == 'y' && set->player->raydir_x > 0)
+			color = put_pixel_to_address(&set->map->west, x, y, set);	
+		else 
+			color = put_pixel_to_address(&set->map->east, x, y, set);
+		set->mlx->mlx_img_addr[set->mlx->size_line * set->player->ply_j + set->player->ply_i * set->mlx->bits_per_pixel / 8] = color;
+		y++;
+		set->player->ply_i++;
+	}
+//	color = 0x00FFFF;
 //	mlx_pixel_put(set->mlx->mlx_init, set->mlx->mlx_window, j * set->mlx->bits_per_pixel / 8, set->mlx->size_line * y, color);
-	set->mlx->mlx_img_addr[set->mlx->size_line * y + j * set->mlx->bits_per_pixel / 8] = color;
 }
 	
 void	get_images(t_setting *set, int j)
 {
-	if (set->player->face_of_cube == 0)
+	int	y = 0;
+	int x = 0;
+	if (set->player->face_of_cube == 'x')
 		set->player->wall_x = set->player->ply_i + set->player->distance * set->player->raydir_y;
 	else
 		set->player->wall_x = set->player->ply_j + set->player->distance * set->player->raydir_x;
@@ -75,7 +82,7 @@ void	get_images(t_setting *set, int j)
 	{
 		set->player->text_y = (int)(set->player->text_pos) & (TEXT_H - 1);
 		set->player->text_pos += set->player->text_step;
-		pixel_by_pixel(set, j, set->player->beginning_of_the_walls); 
+		pixel_by_pixel(set, y++, x++); 
 		(void)j;
 		set->player->beginning_of_the_walls++;
 	}
